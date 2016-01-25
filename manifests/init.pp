495108c1866acd3928683f11bb1b44ac2de063df
Kevin McNally <kevinmcnally26@gmail.com>
2016-01-25 10:13:19 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
-class phpchain (
+class lm_role_phpchain (
 
   $server_name                      = $fqdn,
-  $keypath                          = '/etc/pki/tls/private/phpchain.elab.lastminute.com.key',
-  $certpath                         = '/etc/pki/tls/certs/phpchain.elab.lastminute.com.crt',
-  $phpchain_root                    = '/opt/www/phpchain',
-  $phpchain_base                    = '/opt/www',
+  $keypath                          = undef,
+  $certpath                         = undef,
+  $phpchain_root                    = undef,
+  $phpchain_base                    = undef,
 
-  $phpchain_sitename                = 'Lastminute phpChain',
-  $mysql_rootpw                     = '5CLM1liNRl',
-  $mysql_dbHost                     = 'localhost',
-  $mysql_dbUsername                 = 'phpchain',
-  $mysql_dbPassword                 = '8zomz7uGJ0',
-  $mysql_dbName                     = 'phpchain',
-  $mysql_backupuser                 = 'MrBackups',
-  $mysql_backuppw                   = 'XXaBO7cTI9',
+  $phpchain_sitename                = undef,
+  $mysql_rootpw                     = undef,
+  $mysql_dbHost                     = undef,
+  $mysql_dbUsername                 = undef,
+  $mysql_dbPassword                 = undef,
+  $mysql_dbName                     = undef,
+  $mysql_backupuser                 = undef,
+  $mysql_backuppw                   = undef,
 
 )  {
 
@@ -52,23 +52,23 @@ class { 'apache': }
   }
 
   class { '::mysql::backup::mysqldump': 
-    backupuser      => $mysql_backupuser,
-    backuppassword  => $mysql_backuppw,
-    backupdir       => '/root/phpchain-mysql-backups', 
+    backupuser     => $mysql_backupuser,
+    backuppassword => $mysql_backuppw,
+    backupdir      => '/root/phpchain-mysql-backups', 
     backupdatabases => $mysql_dbName,
     backuprotate    => 7,
     time            => ['21','01'],
   }
 
 ## phpChain
-  file { $phpchain::phpchain_base :
+  file { $lm_role_phpchain::phpchain_base :
     ensure  => directory,
     owner   => 'root',
     group   => 'root',
     mode    => '755',
   }->
 
-  file { "${phpchain::phpchain_base}/phpchain-2.0.11-beta.tgz":
+  file { "${lm_role_phpchain::phpchain_base}/phpchain-2.0.11-beta.tgz":
     owner   => 'apache',
     group   => 'apache',
     mode    => '644',
@@ -76,11 +76,11 @@ class { 'apache': }
   }->
 
   exec { "Extract phpchain source code" :
-    command => "/bin/tar xzf ${phpchain::phpchain_base}/phpchain-2.0.11-beta.tgz -C ${phpchain::phpchain_base}",
-    unless  => "/bin/ls /${phpchain::phpchain_base}/phpchain-2/config",
+    command => "/bin/tar xzf ${lm_role_phpchain::phpchain_base}/phpchain-2.0.11-beta.tgz -C ${lm_role_phpchain::phpchain_base}",
+    unless  => "/bin/ls /${lm_role_phpchain::phpchain_base}/phpchain-2/config",
   }->
 
-  file { "${phpchain::phpchain_base}/phpchain-2":
+  file { "${lm_role_phpchain::phpchain_base}/phpchain-2":
     ensure  => directory,
     recurse => true,
     owner   => 'apache',
@@ -89,25 +89,18 @@ class { 'apache': }
   }->
 
 ## Tar gets extracted as phpchain-2 so symlink
-  file { "${phpchain::phpchain_base}/phpchain":
+  file { "${lm_role_phpchain::phpchain_base}/phpchain":
     ensure => link,
-    target => "${phpchain::phpchain_base}/phpchain-2",
+    target => "${lm_role_phpchain::phpchain_base}/phpchain-2",
   }->
 
 
-  file { "${phpchain::phpchain_base}/phpchain/config/config.php":
+  file { "${lm_role_phpchain::phpchain_base}/phpchain/config/config.php":
     owner   => 'apache',
     group   => 'apache',
     mode    => '644',
     content => template("${module_name}/config.php.erb"),
   }
 
-  package { [ 'php',
-              'php-common',
-              'php-mcrypt',
-              'php-mysql'
-              ]:
-    ensure => installed,
-  }
 
 }
